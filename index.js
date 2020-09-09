860a5c2c989964a85615953bd6f9a568498abf3c
SWFullPotential <swfullpotential@gmail.com>
2020-09-09 15:56:18 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+static capitalize(string) {
+  return (string.charAt(0).toUpperCase() + string.slice(1));
 }
+
+static sanitize(string){
+  return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+}
+
+static titleize(string){
+  let ignore = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+  let splitArray = string.split(" ")
+  let capitalArray = splitArray.map(word => {
+    if(!ignore.includes(word)){
+      return this.capitalize(word)
+    }
+    else {return word}
+  })
+  capitalArray[0] = this.capitalize(capitalArray[0])
+  return capitalArray.join(" ")
+}
+}
+
